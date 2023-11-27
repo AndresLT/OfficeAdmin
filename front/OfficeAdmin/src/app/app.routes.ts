0b38d4bf5d786cc618911515af8bd4906d0da647
Andres Lozano Triana <andres.lozanotriana@dxc.com>
2023-11-27 01:48:24 -0500

    Se agrega label con nombre del usuario a navbar

--- front/OfficeAdmin/src/app/app.routes.ts
+++ front/OfficeAdmin/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './feature/login/login.component';
-import { HomeComponent } from './feature/main/home/home.component';
 import { authGuard } from './core/guards/auth.guard';
 import { loggedInGuard } from './core/guards/logged-in.guard';
 
 export const routes: Routes = [
-  {path: '', loadComponent: () => import('./feature/login/login.component').then(m => m.LoginComponent), canActivate: [loggedInGuard]},
+  {path: '', loadComponent: () => import('./feature/login/login.component').then(m => m.LoginComponent),canActivate:[loggedInGuard]},
   {path: 'home', loadChildren: () => import('./feature/main/main.routes').then(m => m.routes), canActivate: [authGuard]},
   {path: '**', loadChildren: () => import('./feature/main/main.routes').then(m => m.routes), canActivate: [authGuard]}
 ];
